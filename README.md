434fd3d5c332ff273aebae1202e8d9b04acf07b8
noppomario <9283305+noppomario@users.noreply.github.com>
2022-07-06 01:46:56 +0900

    チュートリアル改変

--- README.md
+++ README.md
@@ -1,1 +1,42 @@
-# docusaurus-tutorial
+# Website
+
+This website is built using [Docusaurus 2](https://docusaurus.io/), a modern static website generator.
+
+### Local Development
+
+```
+$ npm start
+```
+
+This command starts a local development server and opens up a browser window. Most changes are reflected live without having to restart the server.
+
+### Build
+
+```
+$ npm build
+```
+
+This command generates static content into the `build` directory and can be served using any static contents hosting service.
+
+### Deployment
+
+Using SSH:
+
+```
+$ USE_SSH=true npm deploy
+```
+
+Not using SSH:
+
+```
+$ GIT_USER=<Your GitHub username> npm deploy
+```
+
+If you are using GitHub pages for hosting, this command is a convenient way to build the website and push to the `gh-pages` branch.
+
+### 参考
+
+- https://qiita.com/y_catch/items/46b7eb7d618d95fbc9c3
+  - search
+- https://dev.to/eleboucher/how-to-deploy-docusaurus-v2-on-gitlab-pages-14e6
+  - deploy
---
+++ docusaurus.config.js
@@ -0,0 +1,69 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'My Site',
+  tagline: 'Dinosaurs are cool',
+  url: 'https://your-docusaurus-test-site.com',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+
+  i18n: {
+    defaultLocale: 'jp',
+    locales: ['jp'],
+  },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          routeBasePath: '/',
+          sidebarPath: require.resolve('./sidebars.js')
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: 'ドキュメント',
+        logo: {
+          alt: 'logo',
+          src: 'img/logo.svg',
+        },
+        items: [
+          {
+            type: 'doc',
+            docId: 'tutorials/intro',
+            position: 'left',
+            label: 'Tutorial',
+          },
+          {
+            type: 'doc',
+            docId: 'api/intro',
+            position: 'left',
+            label: 'API',
+          }
+        ],
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
